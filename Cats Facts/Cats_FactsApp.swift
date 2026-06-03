a9e61b40b8fdf17f2b5e97e44b2afb2a14e241f4
shayahartford <shayahenrique@gmail.com>
2026-06-03 09:06:26 -0300

    Initial Commit

---
+++ Cats Facts/Cats_FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_FactsApp.swift
+//  Cats Facts
+//
+//  Created by Shaya H on 03/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Cats_FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cats FactsTests/Cats_FactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_FactsTests.swift
+//  Cats FactsTests
+//
+//  Created by Shaya H on 03/06/26.
+//
+
+import Testing
+@testable import Cats_Facts
+
+struct Cats_FactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
