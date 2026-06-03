45d396b4b7ab6ca16f1d5870d29fdbb86a22fc34
shayahartford <shayahenrique@gmail.com>
2026-06-03 10:42:01 -0300

    Update ContentView.swift

--- Cats Facts/ContentView.swift
+++ Cats Facts/ContentView.swift
@@ -7,18 +7,53 @@
 
 import SwiftUI
 
+struct CatFact: Codable { //estrutura para ler o JSON
+    let fact: String
+}
+
 struct ContentView: View {
+    
+    @State private var fato = "Nenhum fato carregado" // variável de estado
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack { // (ou EMPILHARV em português) permite juntar dois ou mais conjuntos de dados empilhando-os verticalmente, um abaixo do outro.
+            Text(fato)
+            
+            Button("Buscar fato") {
+                buscarFato()
+                
+            }
         }
         .padding()
     }
+    
+    
+
+    
+    
+    func buscarFato() {
+        let url = URL(string: "https://catfact.ninja/fact")! //isso é a forma como eu acesso a API
+        //fact é um endpoint (a porta de acesso)
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+         
+            guard let data = data else { return } //ve se esta vazio
+            
+            if let resultado = try? JSONDecoder().decode(CatFact.self, from: data) {
+                
+                DispatchQueue.main.async {
+                    fato = resultado.fact
+                }
+                
+            }
+            
+        }.resume()
+    }
+    
 }
 
 #Preview {
-    ContentView()
-}
+        ContentView()
+    }
+    
+
